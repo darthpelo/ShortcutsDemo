b7a593484670f5e37d71f8411a3b20a037988d44
Alessio Roberto <darthpelo@gmail.com>
2018-06-06 22:09:21 +0200

    Initial Commit

---
+++ ShortcutsDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShortcutsDemo
+//
+//  Created by Alessio Roberto on 06/06/2018.
+//  Copyright © 2018 Alessio Roberto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
